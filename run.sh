1816d540c403a14af3d96fadbff9cd0642ea4251
Mark McEver <mark@fastmail.net>
2025-06-02 11:51:27 -0500

    Automatically use a local copy of rctf if it is present

--- run.sh
+++ run.sh
@@ -51,6 +51,11 @@ cd redcap_cypress
 npm install --no-fund
 npm run redcap_rsvc:validate_features
 
+rctfPath="../../rctf/"
+if [ -d "$rctfPath" ]; then
+    npm link $rctfPath
+fi
+
 # Ideally we'd call "npm run redcap_rsvc:move_files" here instead of the following lines,
 # but we can't do that currently because "redcap_rsvc:move_files" contains
 # a "move-cli node_modules/redcap_rsvc redcap_rsvc" command which only makes sense in
