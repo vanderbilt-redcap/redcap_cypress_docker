fff771bca50eba9da5c4ed1270c9724bedbed76c
Mark McEver <mark@fastmail.net>
2025-09-18 15:54:40 -0500

    Reduced the number of similar config files needed

--- download_repos.sh
+++ download_repos.sh
@@ -4,8 +4,8 @@ git clone git@github.com:vanderbilt-redcap/redcap_cypress.git
 git clone git@github.com:vanderbilt-redcap/redcap_docker.git
 
 #Move Base Configurations for Cypress
-cp ./config/cypress.config.js ./redcap_cypress/cypress.config.js
-cp ./config/cypress.env.json ./redcap_cypress/cypress.env.json
+cp ./redcap_cypress/cypress.config.js.example ./redcap_cypress/cypress.config.js
+awk '{ gsub(/"temp_folder": ".*",/, "\"temp_folder\": \"/var/www/html/temp\","); print }' ./redcap_cypress/cypress.env.json.example > ./redcap_cypress/cypress.env.json
 
 #Install the REDCap RSVC repository so automated feature tests can run
 cd redcap_cypress
